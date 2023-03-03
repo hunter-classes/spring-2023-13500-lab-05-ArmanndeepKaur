66050a5d2661f8269343ac33bbd65d7cab0a4539
Armanndeep Kaur <104866931+ArmanndeepKaur@users.noreply.github.com>
2023-03-03 06:58:53 +0000

    added tests

--- .ccls-cache/@home@runner@spring-2023-13500-lab-05-ArmanndeepKaur/tests.cpp
+++ .ccls-cache/@home@runner@spring-2023-13500-lab-05-ArmanndeepKaur/tests.cpp
@@ -4,3 +4,56 @@
 
 // add your tests here
 
+//Task A 
+TEST_CASE("isDivisibleBy")
+{ 
+	CHECK(isDivisibleBy(100,25) == true);
+	CHECK(isDivisibleBy(35,17) == false);
+}	
+
+//Task B 
+TEST_CASE("isPrime")
+{
+  CHECK(isPrime(5) == true);
+	CHECK(isPrime(6) == false);
+	CHECK(isPrime(7) == true);
+}
+
+//Task C
+TEST_CASE("nextPrime")
+{
+	CHECK(nextPrime(14) == 17);
+	CHECK(nextPrime(17) == 19);
+}
+
+//Task D
+TEST_CASE("countPrimes")
+{	
+	CHECK(countPrimes(1, 10) == 4);
+	CHECK(countPrimes(7, 12) == 2);
+}
+
+//Task E 
+TEST_CASE("isTwinPrime")
+{
+	CHECK(isTwinPrime(17) == true);
+	CHECK(isTwinPrime(18) == false);
+}
+
+//Task F
+TEST_CASE("nextTwinPrime")
+{
+	CHECK(nextTwinPrime(14) == 17);
+	CHECK(nextTwinPrime(17) == 19);
+}
+
+//Task G
+TEST_CASE("largestTwinPrime")
+{
+	CHECK(largestTwinPrime(5, 18) == 17);
+	CHECK(largestTwinPrime(1, 31) == 31);
+	CHECK(largestTwinPrime(14, 16) == -1);
+}
+
+
+
